2c8d2d84162e8a45ca015fa04092946b515d53db
ahmedobaid <aobaid2004@outlook.com>
2022-11-21 16:33:44 +0000

    removed recursion and added source for code

--- Coursework/Program.cs
+++ Coursework/Program.cs
@@ -11,39 +11,44 @@ namespace CSharpTutorials
             Console.ReadKey();
             Console.Clear();
             Main();
-        }
+        }//reduces extra lines at end of every application
 
         public static void Main()
         {
-            Console.WriteLine("1: Arithmetic Challenge\n2: Square Root calcualtor\n3: Ceaser Cipher Encrypter\n4: Ceaser Decrypter\n5: Exit");
-            Console.WriteLine("What is the option you wish to access: ");
-            int option = integerValidation();
-            while (option <= 0 || option > 5)
+            while (true) //when a miniapp is finished, the program will return back to the main menu allowing the user to select another mini app or exit
             {
-                Console.WriteLine("Write a suitable menu option!");
-                option = integerValidation();
-            }
+                Console.Clear();
+                Console.WriteLine("1: Arithmetic Challenge\n2: Square Root calcualtor\n3: Ceaser Cipher Encrypter\n4: Ceaser Decrypter\n5: Exit\n");
+                Console.WriteLine("What is the option you wish to access: ");
+                int option = integerValidation();
+                while (option <= 0 || option > 5) //prevent number options outside the range of the given menu options
+                {
+                    Console.WriteLine("Write a suitable menu option!");
+                    option = integerValidation();
+                }
 
-            switch (option) //number that is written by user will dictate which miniapp is called
-            {
-                case 1:
-                    Arithmetic();
-                    break;
+                switch (option) //number that is written by user will dictate which miniapp is called
+                {
+                    case 1:
+                        Arithmetic();
+                        break;
 
-                case 2:
-                    SquareRootCalculator();
-                    break;
+                    case 2:
+                        SquareRootCalculator();
+                        break;
 
-                case 3:
-                    CeaserEncrypt("encrypted");
-                    break;
+                    case 3:
+                        CeaserEncrypt("encrypted");
+                        break;
 
-                case 4:
-                    CeaserEncrypt("decrypted"); //the decrypt program is actually within the Ceaser encrypt function however only runs when we pass the "decrypt" parameter
-                    break;
+                    case 4:
+                        CeaserEncrypt("decrypted"); //the decrypt program is actually within the Ceaser encrypt function however only runs when we pass the "decrypt" parameter
+                        break;
 
-                case 5:
-                    return; //by using return on a functuon that is "void", it will terminate the main method thus terminating the program
+                    case 5:
+                        return; //by using return on a functuon that is "void", it will terminate the main method thus terminating the program
+                }
+
             }
 
         }
@@ -61,7 +66,7 @@ namespace CSharpTutorials
                     temp = int.Parse(input);
                 }
             }
-            return temp; //if the input is an integer, temp is returned into the variable or condition it was called in
+            return temp; //if the input is an integer in the first place, temp is returned into the variable or condition it was called in
         }
 
         public static void Arithmetic()
@@ -76,7 +81,7 @@ namespace CSharpTutorials
             {
                 int valueOne = rnd.Next(1, 11);
                 int valueTwo = rnd.Next(1, 11);
-                int operatorRnd = rnd.Next(0, 2);
+                int operatorRnd = rnd.Next(0, 2); //either 0 or 1 is given randomly 10 times allowing it to randomly choose addition or subtraction questions
 
                 switch (operatorRnd) //switch statement will result in different actualAnswers and outputs to user for if the arithmetic operation is + or -
                 {
@@ -92,6 +97,7 @@ namespace CSharpTutorials
                         else
                         {
                             problemCount++;
+                            correctAnswer++;
                             break;
                         }
 
@@ -116,11 +122,21 @@ namespace CSharpTutorials
 
                 }
 
-            } while (problemCount <= 10);
+            } while (problemCount < 10);
 
             Console.WriteLine("You have gotten {0} out of 10 questions correct\n", correctAnswer);
-            returnToMainMenu();
+            Console.WriteLine("Press any key to return to main menu");
+            Console.ReadKey();
         }
+
+
+        /*  INPUT  EXPECTED OUTCOME  ACTUAL OUTCOME
+         *  1, 1   1                 1    
+         *  -1     N/A               N/A (not valid integer)
+         *  a      N/A               N/A (not integer)    
+         *  5, 2   2.23              2.23
+         */
+
 
         public static void SquareRootCalculator()
         {
@@ -135,7 +151,7 @@ namespace CSharpTutorials
             int squareRootNumber = integerValidation();
             while (squareRootNumber < 0)
             {
-                Console.WriteLine("This is not a valid inpu, write a positive number!");
+                Console.WriteLine("This is not a valid integer, write a positive number!");
                 squareRootNumber = integerValidation();
             }
 
@@ -174,7 +190,8 @@ namespace CSharpTutorials
             }
 
             Console.WriteLine("The square root of {0} to {1} decimal places is {2}\n", squareRootNumber, precision, Math.Round(average, precision));
-            returnToMainMenu();
+            Console.WriteLine("Press any key to return to main menu");
+            Console.ReadKey();
 
 
         }
@@ -182,9 +199,22 @@ namespace CSharpTutorials
         public static void CeaserEncrypt(string encryptOrDecrypt)
         {
             Console.Clear();
+
+            if(encryptOrDecrypt == "encrypted")
+            {
+                Console.WriteLine("Encrypt Text");
+                Console.WriteLine("------------");
+            }
+            else
+            {
+                Console.WriteLine("Decrypt Text");
+                Console.WriteLine("------------");
+            }
+
             //a string that contains all alphabet letters and numbers 0 to 9 and space are written and then converted to a char array
             string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
             char[] alphaChars = Alphabet.ToCharArray();
+            int alphaCharsLength = alphaChars.Length; //length is 37
             
 
             //user inputs their shift as an integer
@@ -194,26 +224,30 @@ namespace CSharpTutorials
             string userMessage;
             char[] userCharacters;
 
-            Console.WriteLine("Write your message to be {0}: ", encryptOrDecrypt); //we concatenate the write line with the value of the parameter to remind the user whether they are encrypting or decryoting
+            Console.WriteLine("Write your message to be {0} ensuring that it contains characters A-Z, 0-9 or a space", encryptOrDecrypt); //we concatenate the write line with the value of the parameter to remind the user whether they are encrypting or decryoting
 
             do
             {
                 userMessage = Console.ReadLine();
                 userMessage = userMessage.ToUpper();
-                if((userMessage.IndexOfAny(alphaChars) != 0))
+                if((userMessage.IndexOfAny(alphaChars) != 0)) 
                 {
                     Console.WriteLine("There are invalid characters in your string!");
                 }
 
-            } while (userMessage.IndexOfAny(alphaChars) != 0);
+            }while (userMessage.IndexOfAny(alphaChars) != 0); //repeatedly asks user for input if the characters in their string do not match what the allowed characters for the application are
             userCharacters = userMessage.ToCharArray();
 
 
-            Console.WriteLine("What is your shift?");
+            Console.WriteLine("What is your shift? (between 1 and 36)");
             do
             {
                 shift = integerValidation();
-            } while (shift < 0 || shift > 36);
+                if(shift <= 0 || shift > 36)
+                {
+                    Console.WriteLine("Invalid Shift!");
+                }
+            }while (shift <= 0 || shift > 36);
 
             for (int i = 0; i < userCharacters.Length; i++) //iterating until we reach the number of charaacters in the users input
             {
@@ -221,13 +255,13 @@ namespace CSharpTutorials
                 int letterIndex = Array.IndexOf(alphaChars, letter);
                 if(encryptOrDecrypt == "encrypted")
                 {
-                   int newLetterPos = (((letterIndex + shift) % 37 + 37) % 37);
-                   char newLetter = alphaChars[newLetterPos];
+                   int newLetterPos = (((letterIndex + shift) % alphaCharsLength) % alphaCharsLength); //https://torstencurdt.com/tech/posts/modulo-of-negative-numbers/ equation for modulo with all numbers
+                    char newLetter = alphaChars[newLetterPos];
                    userCharacters[i] = newLetter;
                 }
                 else //if the parameter is anything else (only other possibility is "decrypt", we move towards a decryption scenario
                 {
-                    int newLetterPos = (((letterIndex - shift) % 37 + 37) % 37);
+                    int newLetterPos = (((letterIndex - shift) % alphaCharsLength) % alphaCharsLength); //https://torstencurdt.com/tech/posts/modulo-of-negative-numbers/ is where I understood how to handle negative numbers when it came to modulo.
                     char newLetter = alphaChars[newLetterPos];
                     userCharacters[i] = newLetter;
                 }
@@ -235,12 +269,8 @@ namespace CSharpTutorials
             }
             string newMessage = String.Join("", userCharacters);
             Console.WriteLine("Your encrypted text is " + newMessage);
-            returnToMainMenu();
-            
- 
-            }
-            
+            Console.WriteLine("Press any key to return to main menu");
+            Console.ReadKey();
+        }            
         }
-
-
     }
